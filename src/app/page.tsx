e5e25417b1cb8a61c47995968bc54e966bada843
belekanych <yuriy.belekanych@gmail.com>
2024-04-30 00:13:50 +0300

    Build improvement.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+const mockUrls = [
+  "https://utfs.io/f/6a67dd35-f31b-4748-839b-022d510ccb43-nqy3t4.jpeg",
+  "https://utfs.io/f/14693e8f-d7ce-45d0-9919-2141d4e0e1e5-v5vqs5.jpeg",
+  "https://utfs.io/f/ba5ace21-7b78-4862-a08f-f5ebfd7d6be4-jk0203.jpg",
+  "https://utfs.io/f/63f65deb-7484-4561-90c0-50c6a32e64e4-hzt98r.jpeg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello World!
+    <main>
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
